4595b263e9daa266cb04f4b702c38ac0711cd1ff
Jason Zhang <zhang.jason@live.com>
2022-09-12 15:38:17 -0400

    Version checker

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from wsgiref.simple_server import sys_version
+
+
 print("Hello World")
 print("Actually Never Mind, Bye World")
 print("swag")
 print("Does python have switches now?")
+print("Python Version: " + sys_version)
 temp = 0
 match temp:
     case 0:

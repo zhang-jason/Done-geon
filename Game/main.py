b021178b96865762228506e333cb8528fd2fd63b
alexanderskladanek <askladanek@ufl.edu>
2022-09-12 17:01:23 -0400

    Created Basic scaling window functionality. Has issue with updating that I'm working on

--- Game/main.py
+++ Game/main.py
@@ -1,12 +1,45 @@
+import os
+from math import floor
+from time import sleep
 from wsgiref.simple_server import sys_version
+import pygame
+
+pygame.display.init()
+display_info = pygame.display.Info()
+width = display_info.current_w
+height = display_info.current_h - 70  # 70 for window headers
+NUM_TILES_X, NUM_TILES_Y = 16, 9
+# 16x9 tiles
+ASSET_SIZE = 32
+scale = min(floor(height/NUM_TILES_Y/ASSET_SIZE), floor(width/NUM_TILES_X/ASSET_SIZE))
+TILE_SIZE = ASSET_SIZE * scale
+print(height)
+print("TILE_SIZE =", TILE_SIZE)
+WIDTH = TILE_SIZE * NUM_TILES_X
+HEIGHT = TILE_SIZE * NUM_TILES_Y
+print(WIDTH)
+print(HEIGHT)  # Just double-checking my math here
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Done-geon")
+
+print("Created Window")
+
+def scale_image(image):
+    return pygame.transform.scale(image, (TILE_SIZE, TILE_SIZE))
 
 
-print("Hello World")
-print("Actually Never Mind, Bye World")
-print("swag")
-print("Does python have switches now?")
-print("Python Version: " + sys_version)
-temp = 0
-match temp:
-    case 0:
-        print("Yes it does!")
+# temporary, to be modularized later:
+STONE_TILE = scale_image(pygame.image.load(os.path.join(os.path.dirname(__file__), 'assets', 'TileAsset32x32.png'))
+                         .convert())
+# to explain what this does, we define the stone tile sprite from the os' current directory/assets/TileAsset...,
+# convert it to usable pygame image object, then load scale it to the biggest factor of 32x32 we can fit in the screen
+print("Created STONE_TILE")
+
+while True:
+    for x in range(NUM_TILES_X):
+        for y in range(NUM_TILES_Y):
+            WIN.blit(STONE_TILE, (x * TILE_SIZE, y * TILE_SIZE))
+    # print("Rendered Map")
+    pygame.display.update()
+    sleep(1)

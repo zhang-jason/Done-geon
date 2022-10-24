10d40bcda6c65565d02c0782a7a84a6b796e5b97
Jason Zhang <zhang.jason@live.com>
2022-10-24 19:54:29 -0400

    CollideGen Update

--- Game/RandomGen/collideGen.py
+++ Game/RandomGen/collideGen.py
@@ -14,17 +14,40 @@ class CollideGen():
         self.wallMap = wallMap
         self.roomIndex = roomIndex
         self.map = self.initMap(len(floorMap[0]), len(floorMap))
-        self.genFloors()
         self.genMap()
 
     def genFloors(self):
-        # Generate edge tile images
+        holeLimit = 2
+        holeCount = 0
+        floorIDs = [9, 10, 11]
+
+        # Generate special floor tiles
         for i in range(1, len(self.floorMap) - 1):
                 for j in range(1, len(self.floorMap[i]) - 1):
                     if self.__isHole__(i, j):
-                        self.map[i][j] = randint(2,4)
+                        genID = choice(floorIDs)
+
+                        if genID == 9:
+                            floorIDs.remove(9)
+                        elif genID == 10:
+                            holeCount += 1
+
+                        if holeCount == holeLimit:
+                            floorIDs.remove(10)
+                            holeCount += 1 # Can never enter this loop again
+                        
+                        self.map[i][j] = genID
+
+    def genObjects(self):
+        return
+
+    def genCluster(self):
+        return
 
     def genMap(self):
+        self.genFloors()
+        self.genObjects()
+
         with open(os.path.join(os.path.dirname(__file__), '..', 'assets/tiles/temprooms', f'room{self.roomIndex}_3.csv'), 'w', newline='') as file:
             writer = csv.writer(file)
             writer.writerows(self.map)
